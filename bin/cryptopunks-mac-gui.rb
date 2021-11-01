3eafd6d8526d715033f4bac5946d2cec7051dec3
Andy Maleh <andy.am@gmail.com>
2021-11-01 17:48:46 -0400

    Mac Packaging Support via Platypus

---
+++ bin/cryptopunks-mac-gui.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+puts File.expand_path('../../ruby-3.0.2@cryptopunks-gui', __dir__)
+
+Gem.paths = {
+  'GEM_HOME' => File.expand_path('../../ruby-3.0.2@cryptopunks-gui', __dir__),
+  'GEM_PATH' => Gem.paths.path + [File.expand_path('../../ruby-3.0.2@cryptopunks-gui', __dir__)],
+  'GEM_SPEC_CACHE' => Gem.paths.spec_cache_dir
+}
+require_relative '../app/cryptopunks_gui.rb'
