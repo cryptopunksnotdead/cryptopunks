61194b5b8caa23e9c38fcacce5f17bf30039deed
Gerald Bauer <gerald.bauer@gmail.com>
2022-12-03 19:47:26 +0100

    up

---
+++ cryptopunks-contracts/README.md
@@ -0,0 +1,27 @@
+#  Query (Read) Crypto Punks Blockchain Contracts / Services via Ethlite
+
+
+
+
+## CryptoPunksTokenUri by 0xTycoon
+
+_The missing tokenURI() for the CryptoPunks_
+
+> This project implements the `tokenURI` function that is missing in the CryptoPunks
+contract.
+>
+> This contract uses the attributes and image data from
+`0x16F5A35647D6F03D5D3da7b35409D65ba03aF3B2`
+which is the "CryptoPunks Data" contract, deployed by Larva Labs, 8th of Aug 2021.
+https://www.larvalabs.com/blog/2021-8-18-18-0/on-chain-cryptopunks
+>
+> Marketplaces, Wallets and contracts can use this convenient contract to fetch metadata about each punk.
+
+see <https://etherscan.io/address/0x4e776fcbb241a0e0ea2904d642baa4c7e171a1e9>
+and <https://github.com/0xTycoon/punks-token-uri>
+
+
+
+
+
+
---
+++ cryptopunks-contracts/sanbox/tokenuri.rb
@@ -0,0 +1,93 @@
+##################################
+#  test try CryptoPunksTokenUri by 0xTycoon
+#
+#  to run use:
+#    $ ruby   sandbox/tokenuri.rb
+
+
+require 'ethlite'
+
+ETH_NODE  = JsonRpc.new( ENV['INFURA_URI'] )
+
+
+tokenuri_eth = '0x4e776fcbb241a0e0ea2904d642baa4c7e171a1e9'
+
+
+##
+#  function tokenURI(uint256 _tokenId) external view returns (string memory);
+tokenuri = Ethlite::ContractMethod.new( 'tokenURI',
+                     inputs: ['uint256'],
+                     outputs: ['string'] )
+
+##
+#  function parseAttributes(uint256 _tokenId) public view returns (string[8] memory)
+## HACK !?
+## change string[8]  to
+##        (string,string,string,string,string,string,string,string)
+##
+##  string[8] decode_abi fails!!!
+##    double check -return string NOT array - decode_abi broken for string array!!!!
+parseAttributes = Ethlite::ContractMethod.new( 'parseAttributes',
+                            inputs: ['uint256'],
+                             outputs: ['(string,string,string,string,string,string,string,string)'] )
+
+##
+#  function getAttributes(uint256 _tokenId) public view returns (string memory);
+getAttributes  =  Ethlite::ContractMethod.new( 'getAttributes',
+                             inputs: ['uint256'],
+                             outputs: ['string'] )
+
+
+
+
+token_ids = (0..3)
+token_ids.each do |token_id|
+   puts "==> parseAttributes(#{token_id}) returns:"
+   res = parseAttributes.do_call( ETH_NODE, tokenuri_eth, [token_id] )
+   puts
+   puts "string[8]:"
+   puts res.class.name
+   pp res
+end
+
+#=> ["Female 2", "Earring", "Blonde Bob", "Green Eye Shadow", "", "", "", ""]
+#=> ["Male 1", "Smile", "Mohawk", "", "", "", "", ""]
+#=> ["Female 3", "Wild Hair", "", "", "", "", "", ""]
+
+
+token_ids.each do |token_id|
+   puts "==> getAttributes(#{token_id}) returns:"
+   str = getAttributes.do_call( ETH_NODE, tokenuri_eth, [token_id] )
+   data = JSON.parse( str )
+   pp data
+end
+
+
+token_ids.each do |token_id|
+   puts "==> tokenURI(#{token_id}) returns:"
+   str = tokenuri.do_call( ETH_NODE, tokenuri_eth, [token_id] )
+   if str.start_with?( 'data:application/json;base64,' )
+      str = str.sub( 'data:application/json;base64,', '' )
+      data = JSON.parse( Base64.decode64( str ) )
+      ## pp data
+      ## extract image
+      ##  "image"=> "data:image/svg+xml;base64
+      str_image = data.delete( 'image' )
+      str_image = str_image.sub( 'data:image/svg+xml;base64,', '' )
+      puts
+      puts str_image
+      image = Base64.decode64( str_image )
+      ## cut-off inline leading data:image/svg+xml;utf8, too
+      image = image.sub( 'data:image/svg+xml;utf8,', '' )
+
+      write_json( "./tmp/punk#{token_id}.json", data )
+      write_text( "./tmp/punk#{token_id}.svg", image )
+   else
+     puts "!! ERROR - expected json base64-encoded; got:"
+     pp str
+     exit 1
+   end
+end
+
+
+puts "bye"
